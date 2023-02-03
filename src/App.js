dc5168a97cf3336e9254ea4f4e73de02a3fa65df
kkafi09 <afi.firda@gmail.com>
2023-02-03 15:37:05 +0700

    upgrade react template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import "./App.css";
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+    <div className="bg-slate-50 w-full min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-5xl font-bold text-blue-400">React JS & TailwindCSS Quickstart</h1>
+      <h3 className="text-2xl">Enjoy Hacking!</h3>
+      <span className="mt-16">
+        Build by
+        <a href="https://github.com/kkafi09" className="font-medium">
+          @kkafi09
+        </a>
+      </span>
     </div>
   );
 }
